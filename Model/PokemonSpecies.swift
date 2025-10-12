10982536064b5fcc81930d75435fd7ea088be2d5
Sanaz Bahmankhah <sanaz.bahmankhah@gmail.com>
2025-10-12 23:25:10 +0100

    feat(model): add PokemonSpecies data model

---
+++ Model/PokemonSpecies.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSpecies.swift
+//  PokemonDemo
+//
+//  Created by sanaz on 10/12/25.
+//
+
+import Foundation
+
+struct PokemonSpecies: Codable {
+    struct FlavorTextEntry: Codable {
+        let flavor_text: String
+        let language: Language
+        struct Language: Codable { let name: String }
+    }
+    let flavor_text_entries: [FlavorTextEntry]
+}
