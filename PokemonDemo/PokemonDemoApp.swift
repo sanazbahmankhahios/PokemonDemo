89ec8fddcbcd7bdb0404b2e755e59cac7f451f9d
Sanaz Bahmankhah <sanaz.bahmankhah@gmail.com>
2025-10-12 14:57:15 +0100

    Initial Commit

---
+++ PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by sanaz on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
