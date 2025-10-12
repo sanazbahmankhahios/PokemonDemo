404cae81c55d968fac9f527cd0bab86014cdef79
Sanaz Bahmankhah <sanaz.bahmankhah@gmail.com>
2025-10-12 22:55:38 +0100

    test(mocks): add MockService for testing

--- Mocks/MockService.swift
+++ Mocks/MockService.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class MockService: PokemonServiceProtocol {
+    
+    func fetchPokemonList(limit: Int, offset: Int) async throws -> [Pokemon] {
+        return [
+            Pokemon(name: "Bulbasaur", url: URL(string: "https://pokeapi.co/api/v2/pokemon/1/")!),
+            Pokemon(name: "Charmander", url: URL(string: "https://pokeapi.co/api/v2/pokemon/4/")!),
+            Pokemon(name: "Squirtle", url: URL(string: "https://pokeapi.co/api/v2/pokemon/7/")!),
+            Pokemon(name: "Pikachu", url: URL(string: "https://pokeapi.co/api/v2/pokemon/25/")!),
+            Pokemon(name: "Jigglypuff", url: URL(string: "https://pokeapi.co/api/v2/pokemon/39/")!),
+            Pokemon(name: "Meowth", url: URL(string: "https://pokeapi.co/api/v2/pokemon/52/")!)
+        ]
+    }
+}
