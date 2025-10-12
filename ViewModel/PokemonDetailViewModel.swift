f8c3ddd40fa21284970084256068f5bc88333006
Sanaz Bahmankhah <sanaz.bahmankhah@gmail.com>
2025-10-12 23:25:10 +0100

    feat(viewmodel): add PokemonDetailViewModel

---
+++ ViewModel/PokemonDetailViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  PokemonDetailViewModel.swift
+//  PokemonDemo
+//
+//  Created by sanaz on 10/12/25.
+//
+
+import Foundation
+
+@MainActor
+final class PokemonDetailViewModel: ObservableObject {
+    @Published var descriptions: [String] = []
+    @Published var name: String
+    @Published var isLoading = false
+    @Published var isFavorite = false
+    private var isProcessing = false
+    
+    var pokemon: Pokemon
+    private let service: PokemonServiceProtocol
+    
+    init(pokemon: Pokemon, service: PokemonServiceProtocol) {
+        self.pokemon = pokemon
+        name = pokemon.name.capitalized
+        self.service = service
+        self.isFavorite = pokemon.isFavorite
+    }
+    
+    func fetchDescription() async {
+        isLoading = true
+        defer { isLoading = false }
+        
+        do {
+            let descriptions = try await service.fetchPokemonDescription(for: pokemon)
+            self.descriptions = descriptions.isEmpty ? ["No description available"] : descriptions
+        } catch {
+            self.descriptions = ["No description available"]
+        }
+    }
+}
