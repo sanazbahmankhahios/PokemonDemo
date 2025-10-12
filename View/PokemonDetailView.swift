ee8bbd633efe53abe483cd2695f896cc0d496a7a
Sanaz Bahmankhah <sanaz.bahmankhah@gmail.com>
2025-10-12 23:25:10 +0100

    feat(view): add PokemonDetailView

---
+++ View/PokemonDetailView.swift
@@ -0,0 +1,62 @@
+//
+//  PokemonDetailView.swift
+//  PokemonDemo
+//
+//  Created by sanaz on 10/12/25.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    @ObservedObject private var viewModel: PokemonDetailViewModel
+    let onFavorite: (Pokemon) -> Void
+    
+    init(viewModel: PokemonDetailViewModel, onFavorite: @escaping (Pokemon) -> Void) {
+        self.viewModel = viewModel
+        self.onFavorite = onFavorite
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 4) {
+                PokemonImageView(pokemon: viewModel.pokemon, height: 100)
+                Text(viewModel.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.bottom, 8)
+            }
+            .padding(.horizontal)
+            VStack(alignment: .leading, spacing: 8) {
+                descriptionView()
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 24)
+            
+            .task {
+                await viewModel.fetchDescription()
+            }
+        }
+    }
+    
+         @ViewBuilder
+    private func descriptionView() -> some View {
+        if viewModel.isLoading {
+            ProgressView("Loading description...")
+                .italic()
+                .foregroundColor(.gray)
+        } else {
+            ForEach(Array(viewModel.descriptions.enumerated()), id: \.offset) { _ , description in
+                Text(description)
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+            }
+        }
+    }
+}
+
+#Preview {
+    PokemonDetailView(viewModel: PokemonDetailViewModel(pokemon: Pokemon(
+        name: "Bulbasaur",
+        url: URL(string: "https://pokeapi.co/api/v2/pokemon/1/")!
+    ), service: MockService()), onFavorite: { _ in })
+}
